fccc07124059341ce9f9a072b726ccc9989d6df4
Omobolaji Durojaiye <99079690+OmobolajiDurojaiye@users.noreply.github.com>
2026-06-16 07:15:42 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import ArticleDetail from './pages/ArticleDetail';
+
+// Helper component to scroll to top on navigation changes
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/article/:slug" element={<ArticleDetail />} />
